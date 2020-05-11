46bba3041fdd4dd136628a041d17332961f04281
fc-anjos <felipe.anjos@usp.br>
2020-05-11 20:04:59 -0300

    add routes and views for edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-
-  resources :users, only: %i[new create]
-
+  resources :users, only: %i[new create edit update]
+  patch 'users/:id', to: 'posts#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
